1fc2b17f4b1f73b3c18a2bf107bc09193643b15d
wjy0929 <wujingyi0929@outlook.com>
2017-09-16 22:40:28 -0400

    Add files via upload

---
+++ src/main/java/com/oose2017/jwu107/hareandhounds/ErrorReason.java
@@ -0,0 +1,14 @@
+package com.oose2017.jwu107.hareandhounds;
+
+/**
+ * Created by jingyiwu on 9/16/17.
+ */
+public class ErrorReason {
+
+    String reason;
+
+    public  ErrorReason(String reason){
+        this.reason = reason;
+    }
+
+}
